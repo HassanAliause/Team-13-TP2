29341f684298ae7c190e9434f3e1a598447a349a
Dievan Chort <dchort.dc@gmail.com>
2023-02-16 16:05:48 +0000

    UPDATED 2 Database and Images

--- Private/Back End/Database & Images/13_bits.sql
+++ Private/Back End/Database & Images/13_bits.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2023 at 04:43 PM
+-- Generation Time: Feb 16, 2023 at 05:04 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -163,6 +163,7 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product_code` int(10) DEFAULT NULL,
   `name` varchar(500) DEFAULT NULL,
+  `description` varchar(5000) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `quantity` int(5) DEFAULT NULL,
   `image_file` varchar(30) NOT NULL,
@@ -174,35 +175,49 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_code`, `name`, `price`, `quantity`, `image_file`, `total`, `key_value`) VALUES
-(1, 7372, '13 Bits Black Keyboard', '160', 10, 'keyboard2-1.jpeg', '500', 22),
-(2, 2828, 'Logitech Gaming Headset (Black)', '60', 4, 'headset1-1.jpeg', '25', 33),
-(4, 1112, '13 Bits Mouse', '8', 0, 'mice1-1.jpeg', '500', 22),
-(5, 4543, 'Patrick Cat Arcade Machine', '10000', 0, 'arcade1-1.jpeg', '20', 11),
-(6, 5622, 'Apple Wireless Magic Keyboard', '200', 25, 'keyboard1-1.jpeg', '200', 22),
-(7, 7373, 'Logitech Gaming Headset (Red)', '60', 25, 'headset2-1.jpeg', '200', 33),
-(9, 7370, '13 Bits Arcade Headset (Dark)', '90', 10, 'headset2-1.jpeg', '50', 33),
-(10, 7372, '13 Bits Arcade Headset (Blue) ', '60', 25, 'headset3-2.jpeg', '200', 22),
-(11, 7373, '13 Bits Retro Keyboard ', '160', 20, 'keyboard4-1.jpeg', '100', 22),
-(12, 6262, 'MacBook Pro M2 1TB', '1260', 25, 'laptop2-1.jpeg', '200', 11),
-(13, 1221, 'Acer Zenbook Gaming Laptop 1TB', '990', 12, 'laptop1-1.jpeg', '300', 11),
-(14, 1221, 'HyperX Wired Mic (Black)', '75', 55, 'mic1-1.jpeg', '500', 33),
-(15, 9445, 'ProSound Audio Microphone with Stand', '150', 20, 'mic2-1.jpeg', '200', 33),
-(16, 8577, 'SandStrom Wireless Mouse', '80', 123, 'mic1-2.jpeg', '500', 22),
-(17, 8324, 'Ergonomic Mouse', '59', 200, 'mic2-1.jpeg', '500', 22),
-(18, 1221, '13 Bits Tower PC (Mini)', '700', 5, 'pc1-1.jpeg', '10', 11),
-(19, 1221, '13 Bits Ultra Gaming PC', '7500', 5, 'pc2-1.jpeg', '10', 22),
-(20, 1254, '13 Bits Mid Range PC', '500', 22, 'pc3-1.jpeg', '200', 11),
-(21, 4628, 'Logitech PC Speakers (Black)', '35', 233, 'speaker1-1.jpeg', '100', 44),
-(22, 2038, '13 Bits RGB System', '99', 44, 'speaker2-1.jpeg', '223', 44),
-(23, 5456, 'Logitech PC Sound System (Black)', '250', 233, 'speaker3-1.jpeg', '43', 44),
-(24, 1221, '13 Bits Theatre PC Speakers', '99', 553, 'speaker4-2.jpeg', '33', 44),
-(25, 1221, '13 Bits Home Sound Bar', '125', 100, 'speaker5-1.jpeg', '22', 44),
-(26, 7272, 'Wired HD Webcam', '45', 233, 'webcam1-1.jpeg', '100', 55),
-(27, 6273, 'Dual HD Webcam', '60', 545, 'webcam2-1.jpeg', '222', 55),
-(28, 3681, '13 Bits Retro Webcam', '25', 233, 'webcam3-1.jpeg', '202', 55),
-(29, 1090, 'Wireless 4K Webcam', '75', 233, 'webcam4-1.jpeg', '100', 55),
-(30, 7765, 'Hama HD Webcam', '25', 111, 'webcam5-1.jpeg', '93', 55);
+INSERT INTO `products` (`id`, `product_code`, `name`, `description`, `price`, `quantity`, `image_file`, `total`, `key_value`) VALUES
+(1, 7372, '13 Bits Black Keyboard', '13 Bits\' own specially developed Black Keyboard Designed for gaming ', '160', 10, 'keyboard2-1.jpeg', '500', 22),
+(2, 2828, 'Logitech Gaming Headset (Black)', '', '60', 4, 'headset1-1.jpeg', '25', 33),
+(4, 1112, '13 Bits Mouse', '', '8', 0, 'mice1-1.jpeg', '500', 22),
+(5, 4543, 'Patrick Cat Arcade Machine', '', '10000', 0, 'arcade1-1.jpeg', '20', 11),
+(6, 5622, 'Apple Wireless Magic Keyboard', '', '200', 25, 'keyboard1-1.jpeg', '200', 22),
+(7, 7373, 'Logitech Gaming Headset (Red)', '', '60', 25, 'headset2-1.jpeg', '200', 33),
+(9, 7370, '13 Bits Arcade Headset (Dark)', '', '90', 10, 'headset2-1.jpeg', '50', 33),
+(10, 7372, '13 Bits Arcade Headset (Blue) ', '', '60', 25, 'headset3-2.jpeg', '200', 22),
+(11, 7373, '13 Bits Retro Keyboard ', '', '160', 20, 'keyboard4-1.jpeg', '100', 22),
+(12, 6262, 'MacBook Pro M2 1TB', '', '1260', 25, 'laptop2-1.jpeg', '200', 11),
+(13, 1221, 'Acer Zenbook Gaming Laptop 1TB', '', '990', 12, 'laptop1-1.jpeg', '300', 11),
+(14, 1221, 'HyperX Wired Mic (Black)', '', '75', 55, 'mic1-1.jpeg', '500', 33),
+(15, 9445, 'ProSound Audio Microphone with Stand', '', '150', 20, 'mic2-1.jpeg', '200', 33),
+(16, 8577, 'SandStrom Wireless Mouse', '', '80', 123, 'mic1-2.jpeg', '500', 22),
+(17, 8324, 'Ergonomic Mouse', '', '59', 200, 'mic2-1.jpeg', '500', 22),
+(18, 1221, '13 Bits Tower PC (Mini)', '', '700', 5, 'pc1-1.jpeg', '10', 11),
+(19, 1221, '13 Bits Ultra Gaming PC', '', '7500', 5, 'pc2-1.jpeg', '10', 22),
+(20, 1254, '13 Bits Mid Range PC', '', '500', 22, 'pc3-1.jpeg', '200', 11),
+(21, 4628, 'Logitech PC Speakers (Black)', '', '35', 233, 'speaker1-1.jpeg', '100', 44),
+(22, 2038, '13 Bits RGB System', '', '99', 44, 'speaker2-1.jpeg', '223', 44),
+(23, 5456, 'Logitech PC Sound System (Black)', '', '250', 233, 'speaker3-1.jpeg', '43', 44),
+(24, 1221, '13 Bits Theatre PC Speakers', '', '99', 553, 'speaker4-2.jpeg', '33', 44),
+(25, 1221, '13 Bits Home Sound Bar', '', '125', 100, 'speaker5-1.jpeg', '22', 44),
+(26, 7272, 'Wired HD Webcam', '', '45', 233, 'webcam1-1.jpeg', '100', 55),
+(27, 6273, 'Dual HD Webcam', '', '60', 545, 'webcam2-1.jpeg', '222', 55),
+(28, 3681, '13 Bits Retro Webcam', '', '25', 233, 'webcam3-1.jpeg', '202', 55),
+(29, 1090, 'Wireless 4K Webcam', '', '75', 233, 'webcam4-1.jpeg', '100', 55),
+(30, 7765, 'Hama HD Webcam', '', '25', 111, 'webcam5-1.jpeg', '93', 55);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(11) NOT NULL,
+  `product_id` int(2) DEFAULT NULL,
+  `image_1` varchar(30) DEFAULT NULL,
+  `image_2` varchar(30) DEFAULT NULL,
+  `image_3` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -256,6 +271,12 @@ ALTER TABLE `products`
   ADD KEY `keyvalue` (`key_value`);
 
 --
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -300,6 +321,12 @@ ALTER TABLE `login_info`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
