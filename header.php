f851e6ba0e2dafe7fd73c5666c6135c2c735c449
Zain Khel <210126981@aston.ac.uk>
2023-02-23 16:02:48 +0000

    updated homepage
    - changed homepage to php file
    - inserted header code
    - took homepage out of homepage folder into main folder
    - deleted empty homepage folder

--- header.php
+++ header.php
@@ -5,13 +5,11 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Contact Us</title>
         <link rel="stylesheet" href="productspage.css">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
-
 
     <div class="logo-header">
         <a href=""><img src="images\logogif4.gif" class="logo" width = 75% alt=""></a>
@@ -22,9 +20,9 @@
         <label for="box" class="boxbtn">
             <i class="fa fa-bars"></i>
         </label>
-        <a href=""><img src="" class="logo" alt=""></a>
+        
         <ul>  
-            <li><a href="">Home</a></li>
+            <li><a href="homepage1.php">Home</a></li>
             <li><a href="productspage.php">Products</a></li>
             <li><a href="contact us.php">Contact Us</a></li>  
             <li><a href="">Logout</a></li>
@@ -34,7 +32,7 @@
             <!-- <li><a href="wishlist"><i class="fa fa-heart-o" style="font-size:25px"></i></a></li>
             <li><a href="cartmenu"><i class="fa fa-shopping-cart" style="font-size:25px"></i></a></li> -->
         
-        </ul>
+        </ul>   
         
     </nav>
         
