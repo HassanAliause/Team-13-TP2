d98271926132ef739de1227a14cb5ebc2ae5e336
Dievan Chort <dchort.dc@gmail.com>
2023-02-27 20:02:42 +0000

    Database FURTHER FIX

--- Private/Back End/Database & Images/13_bits.sql
+++ Private/Back End/Database & Images/13_bits.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 27, 2023 at 08:33 PM
+-- Generation Time: Feb 27, 2023 at 09:01 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -183,7 +183,7 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `quantity`, `image
 (6, 'Apple Wireless Magic Keyboard', '', '200', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261469613301870/keyboard1-1PNG.png', '200', 22),
 (7, 'Logitech Gaming Headset (Red)', '', '60', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261026023706744/headset2-1PNG.png', '200', 33),
 (9, '13 Bits Arcade Headset (Dark)', '', '90', 10, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025801412628/headset1-2PNG.png', '50', 33),
-(10, '13 Bits Arcade Headset (Blue) ', '', '90', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025277124679/headset3-2PNG.png', '200', 22),
+(10, '13 Bits Arcade Headset (Blue) ', '', '90', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025277124679/headset3-2PNG.png', '200', 33),
 (11, '13 Bits Retro Keyboard ', '', '160', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261470271807498/keyboard4-1PNG.png', '100', 22),
 (12, 'MacBook Pro M2 1TB', 'Powered by the next-generation M2 chip, the redesigned MacBook Air packs incredible performance and up to 18 hours of battery life into an incredibly thin aluminum body.', '1260', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1075794599307649095/laptop2-1.png', '200', 11),
 (13, 'Acer Zenbook Gaming Laptop 1TB', 'Gaming Laptop created by Acer with 1TB SSD ', '990', 12, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261430795026462/laptop1-1PNG.png', '300', 11),
@@ -192,7 +192,7 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `quantity`, `image
 (16, 'SandStrom Wireless Mouse', '', '80', 123, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336695820368/mice1-2PNG.png', '500', 22),
 (17, 'Ergonomic Mouse', '', '59', 200, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336444145715/mice2-1PNG.png', '500', 22),
 (18, '13 Bits Tower PC (Mini)', '', '700', 5, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277652594858/pc1-1PNG.png', '10', 11),
-(19, '13 Bits Ultra Gaming PC', '', '7500', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261278768283718/pc2-1PNG.png', '10', 22),
+(19, '13 Bits Ultra Gaming PC', '', '7500', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261278768283718/pc2-1PNG.png', '10', 11),
 (20, '13 Bits Mid Range PC', '', '500', 22, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277988143114/pc3-1PNG.png', '200', 11),
 (21, 'Logitech PC Speakers (Black)', '', '35', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261233729839184/speaker1-1PNG.png', '100', 44),
 (22, '13 Bits RGB System', '', '99', 44, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261235306909796/speaker2-1PNG.png', '223', 44),
@@ -201,8 +201,8 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `quantity`, `image
 (26, 'Wired HD Webcam', '', '45', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261164481892382/webcam1-1PNG.png', '100', 55),
 (27, 'Dual HD Webcam', '', '60', 545, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261166268653659/webcam2-1PNG.png', '222', 55),
 (28, '13 Bits Retro Webcam', '', '25', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261165555621979/webcam3-1PNG.png', '202', 55),
-(29, 'Wireless 4K Webcam', '', '75', 233, 'webcam4-1.jpeg', '100', 55),
-(30, 'Hama HD Webcam', '', '25', 111, 'webcam5-1.jpeg', '93', 55);
+(31, 'Wireless 4K Webcam', 'a High-end 4K webcam designed by 13 bits to provide the best resolution towards recording. Also optimised for streaming', '75', 1, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261165777932379/webcam2-3PNG.png', '20', 22),
+(32, 'Hama HD Webcam', '1080p Streaming camera providing a constant high resolution feed', '25', 0, 'https://media.discordapp.net/attachments/1073347102559703130/1077261164918087810/webcam5-1PNG.png', '40', 22);
 
 -- --------------------------------------------------------
 
@@ -258,8 +258,7 @@ ALTER TABLE `customer_details`
 --
 ALTER TABLE `customer_orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `product` (`product`),
-  ADD KEY `name` (`name`);
+  ADD KEY `product` (`product`);
 
 --
 -- Indexes for table `login_info`
@@ -278,7 +277,6 @@ ALTER TABLE `message`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name` (`name`),
   ADD KEY `keyvalue` (`key_value`);
 
 --
@@ -331,7 +329,7 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `product_images`
