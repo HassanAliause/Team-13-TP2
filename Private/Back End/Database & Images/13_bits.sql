b0fa0f03b1429a253ee46fd045cdb8998dff53cc
Dievan Chort <dchort.dc@gmail.com>
2023-02-27 19:35:06 +0000

    Updated Database FIXED

--- Private/Back End/Database & Images/13_bits.sql
+++ Private/Back End/Database & Images/13_bits.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 27, 2023 at 07:24 PM
+-- Generation Time: Feb 27, 2023 at 08:33 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -163,6 +163,7 @@ CREATE TABLE `message` (
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
+  `description` varchar(3000) NOT NULL,
   `price` decimal(10,0) NOT NULL,
   `quantity` int(5) DEFAULT NULL,
   `image_file` varchar(3000) NOT NULL,
@@ -174,34 +175,34 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `image_file`, `total`, `key_value`) VALUES
-(1, 'HP Wireless Black Keyboard', '160', 10, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png', '500', 22),
-(2, 'Logitech Gaming Headset (Black)', '60', 4, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025507807303/headset1-1PNG.png', '25', 33),
-(4, '13 Bits Mouse', '8', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261335907291147/mice1-1PNG.png', '500', 22),
-(5, 'Patrick Cat Arcade Machine', '10000', 5, 'https://cdn.discordapp.com/attachments/1073347102559703130/1078034168732209262/arcade1-1.jpeg', '20', 11),
-(6, 'Apple Wireless Magic Keyboard', '200', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261469613301870/keyboard1-1PNG.png', '200', 22),
-(7, 'Logitech Gaming Headset (Red)', '60', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261026023706744/headset2-1PNG.png', '200', 33),
-(9, '13 Bits Arcade Headset (Dark)', '90', 10, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025801412628/headset1-2PNG.png', '50', 33),
-(10, '13 Bits Arcade Headset (Blue) ', '60', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025277124679/headset3-2PNG.png', '200', 22),
-(11, '13 Bits Retro Keyboard ', '160', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261470271807498/keyboard4-1PNG.png', '100', 22),
-(12, 'MacBook Pro M2 1TB', '1260', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1075794599307649095/laptop2-1.png', '200', 11),
-(13, 'Acer Zenbook Gaming Laptop 1TB', '990', 12, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261430795026462/laptop1-1PNG.png', '300', 11),
-(14, 'HyperX Wired Mic (Black)', '75', 55, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261381973311488/mic1-1PNG.png', '500', 33),
-(15, 'ProSound Audio Microphone with Stand', '150', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261382380167178/mic2-2PNG.png', '200', 33),
-(16, 'SandStrom Wireless Mouse', '80', 123, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336695820368/mice1-2PNG.png', '500', 22),
-(17, 'Ergonomic Mouse', '59', 200, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336444145715/mice2-1PNG.png', '500', 22),
-(18, '13 Bits Tower PC (Mini)', '700', 5, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277652594858/pc1-1PNG.png', '10', 11),
-(19, '13 Bits Ultra Gaming PC', '7500', 5, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261278768283718/pc2-1PNG.png', '10', 22),
-(20, '13 Bits Mid Range PC', '500', 22, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277988143114/pc3-1PNG.png', '200', 11),
-(21, 'Logitech PC Speakers (Black)', '35', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261233729839184/speaker1-1PNG.png', '100', 44),
-(22, '13 Bits RGB System', '99', 44, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261235306909796/speaker2-1PNG.png', '223', 44),
-(23, 'Logitech PC Sound System (Black)', '250', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261234409316362/speaker4-1PNG.png', '43', 44),
-(24, 'Logitech Theatre PC Speakers', '99', 553, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261234602266744/speaker3-1PNG.png', '33', 44),
-(26, 'Wired HD Webcam', '45', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261164481892382/webcam1-1PNG.png', '100', 55),
-(27, 'Dual HD Webcam', '60', 545, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261166268653659/webcam2-1PNG.png', '222', 55),
-(28, '13 Bits Retro Webcam', '25', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261165555621979/webcam3-1PNG.png', '202', 55),
-(29, 'Wireless 4K Webcam', '75', 233, 'webcam4-1.jpeg', '100', 55),
-(30, 'Hama HD Webcam', '25', 111, 'webcam5-1.jpeg', '93', 55);
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `quantity`, `image_file`, `total`, `key_value`) VALUES
+(1, 'HP Wireless Black Keyboard', 'Black Wireless Keyboard by HP, using a USB connection to provide the link between your PC and the keyboard', '160', 10, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png', '500', 22),
+(2, 'Logitech Gaming Headset (Black)', '5.1 Surround Sound, over 30 hours of Battery with RGB capabilities', '60', 4, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025507807303/headset1-1PNG.png', '25', 33),
+(4, '13 Bits Mouse', 'A wireless mouse with an ergonomic design and fast responses to clicks', '8', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261335907291147/mice1-1PNG.png', '500', 22),
+(5, 'Patrick Cat Arcade Machine', 'One of the greatest pieces of machinery to ever grace Planet Earth. High-end Arcade machine containing cross-platform access of video games', '10000', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1078034168732209262/arcade1-1.jpeg', '20', 11),
+(6, 'Apple Wireless Magic Keyboard', '', '200', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261469613301870/keyboard1-1PNG.png', '200', 22),
+(7, 'Logitech Gaming Headset (Red)', '', '60', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261026023706744/headset2-1PNG.png', '200', 33),
+(9, '13 Bits Arcade Headset (Dark)', '', '90', 10, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025801412628/headset1-2PNG.png', '50', 33),
+(10, '13 Bits Arcade Headset (Blue) ', '', '90', 25, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025277124679/headset3-2PNG.png', '200', 22),
+(11, '13 Bits Retro Keyboard ', '', '160', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261470271807498/keyboard4-1PNG.png', '100', 22),
+(12, 'MacBook Pro M2 1TB', 'Powered by the next-generation M2 chip, the redesigned MacBook Air packs incredible performance and up to 18 hours of battery life into an incredibly thin aluminum body.', '1260', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1075794599307649095/laptop2-1.png', '200', 11),
+(13, 'Acer Zenbook Gaming Laptop 1TB', 'Gaming Laptop created by Acer with 1TB SSD ', '990', 12, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261430795026462/laptop1-1PNG.png', '300', 11),
+(14, 'HyperX Wired Mic (Black)', '', '75', 55, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261381973311488/mic1-1PNG.png', '500', 33),
+(15, 'ProSound Audio Microphone with Stand', '', '150', 20, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261382380167178/mic2-2PNG.png', '200', 33),
+(16, 'SandStrom Wireless Mouse', '', '80', 123, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336695820368/mice1-2PNG.png', '500', 22),
+(17, 'Ergonomic Mouse', '', '59', 200, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336444145715/mice2-1PNG.png', '500', 22),
+(18, '13 Bits Tower PC (Mini)', '', '700', 5, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277652594858/pc1-1PNG.png', '10', 11),
+(19, '13 Bits Ultra Gaming PC', '', '7500', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261278768283718/pc2-1PNG.png', '10', 22),
+(20, '13 Bits Mid Range PC', '', '500', 22, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261277988143114/pc3-1PNG.png', '200', 11),
+(21, 'Logitech PC Speakers (Black)', '', '35', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261233729839184/speaker1-1PNG.png', '100', 44),
+(22, '13 Bits RGB System', '', '99', 44, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261235306909796/speaker2-1PNG.png', '223', 44),
+(23, 'Logitech PC Sound System (Black)', '', '250', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261234409316362/speaker4-1PNG.png', '43', 44),
+(24, 'Logitech Theatre PC Speakers', '', '99', 0, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261234602266744/speaker3-1PNG.png', '33', 44),
+(26, 'Wired HD Webcam', '', '45', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261164481892382/webcam1-1PNG.png', '100', 55),
+(27, 'Dual HD Webcam', '', '60', 545, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261166268653659/webcam2-1PNG.png', '222', 55),
+(28, '13 Bits Retro Webcam', '', '25', 233, 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261165555621979/webcam3-1PNG.png', '202', 55),
+(29, 'Wireless 4K Webcam', '', '75', 233, 'webcam4-1.jpeg', '100', 55),
+(30, 'Hama HD Webcam', '', '25', 111, 'webcam5-1.jpeg', '93', 55);
 
 -- --------------------------------------------------------
 
@@ -212,8 +213,6 @@ INSERT INTO `products` (`id`, `name`, `price`, `quantity`, `image_file`, `total`
 CREATE TABLE `product_images` (
   `id` int(11) NOT NULL,
   `product_id` varchar(30) DEFAULT NULL,
-  `name` varchar(100) NOT NULL,
-  `description` varchar(3000) NOT NULL,
   `image_1` varchar(3000) DEFAULT NULL,
   `image_2` varchar(3000) DEFAULT NULL,
   `image_3` varchar(3000) DEFAULT NULL
@@ -223,12 +222,12 @@ CREATE TABLE `product_images` (
 -- Dumping data for table `product_images`
 --
 
-INSERT INTO `product_images` (`id`, `product_id`, `name`, `description`, `image_1`, `image_2`, `image_3`) VALUES
-(1, '1', 'HP Wireless Black Keyboard', 'Black Wireless Keyboard by HP, using a USB connection to provide the link between your PC and the keyboard', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png        ', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261471949533214/keyboard2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png        '),
-(2, '2', 'Logitech Gaming Headset (Black)', '5.1 Surround Sound, over 30 hours of Battery with RGB capabilities', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025507807303/headset1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261026514448434/headset2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025801412628/headset1-2PNG.png'),
-(3, '16', 'SandStrom Wireless Mouse', 'A wireless mouse with an ergonomic design and fast responses to clicks', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261335907291147/mice1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336695820368/mice1-2PNG.png', ''),
-(4, '12', 'MacBook Pro M2 1TB', 'Powered by the next-generation M2 chip, the redesigned MacBook Air packs incredible performance and up to 18 hours of battery life into an incredibly thin aluminum body. ', 'https://cdn.discordapp.com/attachments/1073347102559703130/1075794599307649095/laptop2-1.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261431566762087/laptop2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261431105396736/laptop2-3PNG.png'),
-(5, '13', 'Acer Zenbook Gaming Laptop 1TB', 'Gaming Laptop', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261430795026462/laptop1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261432313348156/laptop1-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261432057503784/laptop1-3PNG.png');
+INSERT INTO `product_images` (`id`, `product_id`, `image_1`, `image_2`, `image_3`) VALUES
+(1, '1', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png        ', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261471949533214/keyboard2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261488336687245/keyboard2-1PNG.png        '),
+(2, '2', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025507807303/headset1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261026514448434/headset2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261025801412628/headset1-2PNG.png'),
+(3, '16', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261335907291147/mice1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261336695820368/mice1-2PNG.png', ''),
+(4, '12', 'https://cdn.discordapp.com/attachments/1073347102559703130/1075794599307649095/laptop2-1.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261431566762087/laptop2-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261431105396736/laptop2-3PNG.png'),
+(5, '13', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261430795026462/laptop1-1PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261432313348156/laptop1-2PNG.png', 'https://cdn.discordapp.com/attachments/1073347102559703130/1077261432057503784/laptop1-3PNG.png');
 
 --
 -- Indexes for dumped tables
