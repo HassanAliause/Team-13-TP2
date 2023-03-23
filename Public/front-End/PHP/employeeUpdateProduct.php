2b7e9d9e9efd1c8eeb26436bbe94a9187263409e
Zain Khel <210126981@aston.ac.uk>
2023-03-23 15:00:56 +0000

    created update php files
    created update php files

---
+++ Public/front-End/PHP/employeeUpdateProduct.php
@@ -0,0 +1,14 @@
+<?php
+    include('connect.php');
+    // include('checkLogin.php');
+
+    $id = $row['id'];
+    $image = $row['image_file'];
+    $name = $row['name'];
+    $description = $row['description'];
+    $price = $row['price']; 
+    $password = $row['quantity'];
+    $total = $row['total'];
+    $keyvalue = $row['key_value'];
+
+?>
