b5160b0b6565352e4a1453723f7569f50ddfd220
HassanAliause <116088371+HassanAliause@users.noreply.github.com>
2023-04-03 04:39:59 +0100

    Update config.php
    updated config for deployment

--- Public/front-End/PHP/config.php
+++ Public/front-End/PHP/config.php
@@ -3,9 +3,9 @@
 
     // Database info
     $DB_HOST = 'localhost';
-    $DB_USER = 'root';
-    $DB_PASS = '';
-    $DB_NAME = '13_bits';
+    $DB_USER = 'u-210142176';
+    $DB_PASS = 'sKtumIb207EYMQW';
+    $DB_NAME = 'u_210142176_db';
 // trying to connect to DB
     $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
     if ( mysqli_connect_errno() ) {
